b8f944ef6857ed6753c3c13a38e24021eee86fce
Babukuruvila <bobbyjohnamion@gmail.com>
2024-07-03 17:50:47 +0530

    edit

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQ-The-File-Donor
+  git clone https://github.com/Babukuruvila/new-repo-web.git /new-repo-web
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-The-File-Donor
+  git clone $UPSTREAM_REPO /new-repo-web
 fi
 cd /DQ-The-File-Donor
 pip3 install -U -r requirements.txt
